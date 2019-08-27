b50bd2c8f65d822c00e74bcb2bea59676e400ecf
Emanuela Davelli <emanuela.davelli@sottosviluppo.com>
2019-08-27 12:09:40 +0200

    bugfix images traits

--- src/Elcodi/Component/Media/Entity/Traits/ImagesContainerTrait.php
+++ src/Elcodi/Component/Media/Entity/Traits/ImagesContainerTrait.php
@@ -22,127 +22,122 @@ use Doctrine\Common\Collections\ArrayCollection;
 /**
  * Trait ImagesContainerTrait.
  */
-trait ImagesContainerTrait
-{
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     *
-     * Images
-     */
-    protected $images;
+trait ImagesContainerTrait {
+	/**
+	 * @var \Doctrine\Common\Collections\Collection
+	 *
+	 * Images
+	 */
+	protected $images;
 
-    /**
-     * @var string
-     *
-     * Images sort
-     */
-    protected $imagesSort;
+	/**
+	 * @var string
+	 *
+	 * Images sort
+	 */
+	protected $imagesSort;
 
-    /**
-     * Set add image.
-     *
-     * @param \Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image Image object to be added
-     *
-     * @return $this Self object
-     */
-    public function addImage(\Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image)
-    {
-        $this->images->add($image);
+	/**
+	 * Set add image.
+	 *
+	 * @param \Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image Image object to be added
+	 *
+	 * @return $this Self object
+	 */
+	public function addImage(\Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image) {
+		if ($this->images == null) {
+			$this->images = new ArrayCollection();
+		}
+		$this->images->add($image);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get if entity is enabled.
-     *
-     * @param \Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image Image object to be removed
-     *
-     * @return $this Self object
-     */
-    public function removeImage(\Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image)
-    {
-        $this->images->removeElement($image);
+	/**
+	 * Get if entity is enabled.
+	 *
+	 * @param \Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image Image object to be removed
+	 *
+	 * @return $this Self object
+	 */
+	public function removeImage(\Elcodi\Component\Media\Entity\Interfaces\ImageInterface $image) {
+		$this->images->removeElement($image);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get all images.
-     *
-     * @return \Doctrine\Common\Collections\ArrayCollection
-     */
-    public function getImages()
-    {
-        return $this->images;
-    }
+	/**
+	 * Get all images.
+	 *
+	 * @return \Doctrine\Common\Collections\ArrayCollection
+	 */
+	public function getImages() {
+		return $this->images;
+	}
 
-    /**
-     * Get sorted images.
-     *
-     * @return \Doctrine\Common\Collections\ArrayCollection
-     */
-    public function getSortedImages()
-    {
-        $imagesSort = explode(',', $this->getImagesSort());
-        $orderCollection = array_reverse($imagesSort);
-        $imagesCollection = $this
-            ->getImages()
-            ->toArray();
+	/**
+	 * Get sorted images.
+	 *
+	 * @return \Doctrine\Common\Collections\ArrayCollection
+	 */
+	public function getSortedImages() {
+		$imagesSort = explode(',', $this->getImagesSort());
+		$orderCollection = array_reverse($imagesSort);
+		$imagesCollection = $this
+			->getImages()
+			->toArray();
 
-        usort(
-            $imagesCollection,
-            function (
-                \Elcodi\Component\Media\Entity\Interfaces\ImageInterface $a,
-                \Elcodi\Component\Media\Entity\Interfaces\ImageInterface $b
-            ) use ($orderCollection) {
+		usort(
+			$imagesCollection,
+			function (
+				\Elcodi\Component\Media\Entity\Interfaces\ImageInterface $a,
+				\Elcodi\Component\Media\Entity\Interfaces\ImageInterface $b
+			) use ($orderCollection) {
 
-                $aPos = array_search($a->getId(), $orderCollection);
-                $bPos = array_search($b->getId(), $orderCollection);
+				$aPos = array_search($a->getId(), $orderCollection);
+				$bPos = array_search($b->getId(), $orderCollection);
 
-                return ($aPos < $bPos)
-                    ? 1
-                    : -1;
-            }
-        );
+				return ($aPos < $bPos)
+				? 1
+				: -1;
+			}
+		);
 
-        return new ArrayCollection($imagesCollection);
-    }
+		return new ArrayCollection($imagesCollection);
+	}
 
-    /**
-     * Set images.
-     *
-     * @param \Doctrine\Common\Collections\ArrayCollection $images Images
-     *
-     * @return $this Self object
-     */
-    public function setImages(\Doctrine\Common\Collections\ArrayCollection $images)
-    {
-        $this->images = $images;
+	/**
+	 * Set images.
+	 *
+	 * @param \Doctrine\Common\Collections\ArrayCollection $images Images
+	 *
+	 * @return $this Self object
+	 */
+	public function setImages(\Doctrine\Common\Collections\ArrayCollection $images) {
+		$this->images = $images;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get ImagesSort.
-     *
-     * @return string ImagesSort
-     */
-    public function getImagesSort()
-    {
-        return $this->imagesSort;
-    }
+	/**
+	 * Get ImagesSort.
+	 *
+	 * @return string ImagesSort
+	 */
+	public function getImagesSort() {
+		return $this->imagesSort;
+	}
 
-    /**
-     * Sets ImagesSort.
-     *
-     * @param string $imagesSort ImagesSort
-     *
-     * @return $this Self object
-     */
-    public function setImagesSort($imagesSort)
-    {
-        $this->imagesSort = $imagesSort;
+	/**
+	 * Sets ImagesSort.
+	 *
+	 * @param string $imagesSort ImagesSort
+	 *
+	 * @return $this Self object
+	 */
+	public function setImagesSort($imagesSort) {
+		$this->imagesSort = $imagesSort;
 
-        return $this;
-    }
+		return $this;
+	}
 }
